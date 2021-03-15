756a313d2f7ec7634209a5c55f20b98896eba066
Vibs <vibej@hotmail.com>
2021-03-15 14:51:01 +0100

    create-user.html lavet

---
+++ js/KinoXP/create-user.js
@@ -0,0 +1,116 @@
+
+
+// vi gemmer vores inputfelter i const-vars
+const inputName = document.getElementById("name");
+const inputEmail = document.getElementById("email");
+const inputPassword = document.getElementById("password");
+const inputConfirmPassword = document.getElementById("comfirmPassword");
+
+// vi gemmer vores pb i en var
+const pbCreateUser = document.getElementById("pbCreateUser");
+
+// vi opretter variabler, hvori vi gemmer det indtastede input fra input-felterne
+let name;
+let email;
+let password;
+let confirmPassword;
+
+const url = "http://localhost:8080/customers";
+
+pbCreateUser.addEventListener('click', insertUserIntoDb);
+
+function insertUserIntoDb(){
+
+  // vi henter værdier fra inputfelterne og sætter vores variabler
+  retrieveInput();
+
+  if(password === confirmPassword) {
+    // vi opretter et JSON-obj ud fra inputfelters værdier
+    body = createJSONPerson(name, email, password);
+
+
+
+    /*
+    * Vi laver nogle specifikationer til vores request
+    * */
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', // betyder == vi sender et json i string-format
+      },
+      body: body
+    };
+
+    fetch(url, requestOptions)
+      .then(reponse => reponse.json())
+      .then(data => console.log("succes: ", data))
+      .catch(error => console.log("error: ", error));
+  }
+
+}
+
+function retrieveInput(){
+  name = inputName.value;
+  email = inputEmail.value;
+  password = inputPassword.value;
+  confirmPassword = inputConfirmPassword.value;
+}
+
+/**
+ * Opretter JSON-string ud fra name, email, password
+ * */
+function createJSONPerson(name, email, password){
+  // vi laver et object (JSON-obj er standard obj i js)
+  let user = {
+    "name": name,
+    "email": email,
+    "password": password
+  };
+
+  /* Vi laver JSON om til en String
+     Fordi at body i requestOptions'en skal angives som string
+   */
+  return JSON.stringify(user);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
