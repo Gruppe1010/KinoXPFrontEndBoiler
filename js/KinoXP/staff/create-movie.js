501be14fffe5f81ac9b6a8e2054336fcd2e5ee2d
Vibs <vibej@hotmail.com>
2021-03-17 15:17:20 +0100

    create-movie.js - man kan nu lægge en film med billede ned i db

--- js/KinoXP/staff/create-movie.js
+++ js/KinoXP/staff/create-movie.js
@@ -8,23 +8,33 @@ let base64;
 function createMovie(){
 
   const moviePoster = document.getElementById('moviePoster').files[0];
-  const title = document.getElementById('title');
+  const title = document.getElementById('title').value;
 
   const moviePromise = getBase64(moviePoster);
 
   moviePromise.then(function(result) {
     base64 = result;
 
+    // TODO den skal tage alle movie-param
+    const body = createJSONMovie(base64, title);
 
+    //Vi laver nogle specifikationer til vores request
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', // betyder == vi sender et json i string-format
+      },
+      body: body
+    };
 
+    const url = 'http://localhost:8080/movies';
 
+    fetch(url, requestOptions)
+      .then(response => response.json())
+      .then(data =>  checkIfSuccess(data))
+      .catch(error => console.log("error: ", error));
 
-
-
-
-    console.log(base64);
   });
-
 
 }
 
@@ -38,3 +48,33 @@ function getBase64(file) {
     reader.readAsDataURL(file);
   });
 }
+
+// TODO rediger så den tager alle params
+function createJSONMovie(base64, title){
+
+  const movie = {
+    "base64": base64,
+    "title": title
+  }
+
+  return JSON.stringify(movie);
+}
+
+function checkIfSuccess(data){
+
+  if(data.id !== 0){
+    console.log("succes: " + data);
+  }
+  else{
+    console.log("error in checkIfSuccess");
+  }
+}
+
+
+
+
+
+
+
+
+
