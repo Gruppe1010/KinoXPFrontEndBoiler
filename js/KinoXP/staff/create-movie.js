a15e82852ac1e4fca447417c82abf2cdaeec36c6
Vibs <vibej@hotmail.com>
2021-03-17 14:55:01 +0100

    vi kan nu uploade et image på create-movie siden og omdanne det til en Base64-String

--- js/KinoXP/staff/create-movie.js
+++ js/KinoXP/staff/create-movie.js
@@ -1,5 +1,4 @@
 
-
 const pbSubmitMovie = document.getElementById('pbSubmitMovie');
 
 pbSubmitMovie.addEventListener('click', createMovie);
@@ -11,57 +10,31 @@ function createMovie(){
   const moviePoster = document.getElementById('moviePoster').files[0];
   const title = document.getElementById('title');
 
-  base64 = getBase64(moviePoster);
+  const moviePromise = getBase64(moviePoster);
+
+  moviePromise.then(function(result) {
+    base64 = result;
 
 
-// hej
+
+
+
+
+
+
+    console.log(base64);
+  });
+
 
 }
 
-console.log(base64);
+
 
 function getBase64(file) {
-  var reader = new FileReader();
-  reader.readAsDataURL(file);
-  reader.onload = function () {return reader.result;};
-  reader.onerror = function (error) {
-    console.log('Error: ', error);
-  };
+  return new Promise(function(resolve, reject) {
+    var reader = new FileReader();
+    reader.onload = function() { resolve(reader.result); };
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
 }
-
-/*
-
-var file = document.querySelector('#files > input[type="file"]').files[0];
-getBase64(file); // prints the base64 string
-
-
-
-
-
-
-
-
-document.getElementById('upload').addEventListener('change', showImage);
-
-function showImage(evt) {
-  var files = evt.target.files;
-
-  if (files.length === 0) {
-    console.log('No files selected');
-    return;
-  }
-
-  var reader = new FileReader();
-  reader.onload = function(event) {
-    var img = new Image();
-    img.onload = function() {
-      document.body.appendChild(img);
-    };
-    img.src = event.target.result;
-  };
-  reader.readAsDataURL(files[0]);
-}
-
-
-
- */
