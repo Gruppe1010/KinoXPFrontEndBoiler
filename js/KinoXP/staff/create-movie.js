c447d73a733e5c892be133619d529607e0c3f772
Vibs <vibej@hotmail.com>
2021-03-18 10:22:13 +0100

    // sikring for at det KUN er staff der kan være på denne side på create-movie

--- js/KinoXP/staff/create-movie.js
+++ js/KinoXP/staff/create-movie.js
@@ -1,113 +1,128 @@
 
-const pbSubmitMovie = document.getElementById('pbSubmitMovie');
+// sikring for at det KUN er staff der kan være på denne side
+const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
 
-pbSubmitMovie.addEventListener('click', createMovie);
+if(user.id !== 2){
+  alert("Du er ikke den rigtige brugertype for at være på denne side");
 
-let base64;
-
-/*
-
-
-    <label htmlFor="length">Længde i minutter</label>
-
-      <label htmlFor="premiere">Premieredato</label>
-        <label htmlFor="yearOfRelease">Udkommelsesår</label>
-
-          <!-- radio button-->
-          <label htmlFor="ageLimit1">Tilladt for alle</label>
-
- */
+  window.location.replace='../general/index.html';
+}
 
 
-function createMovie(){
 
-  const moviePoster = document.getElementById('moviePoster').files[0];
-  const title = document.getElementById('title').value;
+else
+{
 
-  const length = document.getElementById('length').value;
-  const yearOfRelease = document.getElementById('yearOfRelease').value;
-  const ageLimit = document.querySelectorAll("input[name=ageLimit]:checked")[0].value;
+  const pbSubmitMovie = document.getElementById('pbSubmitMovie');
 
-  console.log(ageLimit);
+  pbSubmitMovie.addEventListener('click', createMovie);
+
+  let base64;
+
   /*
-  if (document.getElementById('r1').checked) {
-    rate_value = document.getElementById('r1').value;
-  }
+
+
+      <label htmlFor="length">Længde i minutter</label>
+
+        <label htmlFor="premiere">Premieredato</label>
+          <label htmlFor="yearOfRelease">Udkommelsesår</label>
+
+            <!-- radio button-->
+            <label htmlFor="ageLimit1">Tilladt for alle</label>
 
    */
 
 
-  const moviePromise = getBase64(moviePoster);
+  function createMovie() {
 
-  moviePromise.then(function(result) {
-    base64 = result;
+    const moviePoster = document.getElementById('moviePoster').files[0];
+    const title = document.getElementById('title').value;
 
-    // TODO den skal tage alle movie-param
-    const body = createJSONMovie(base64, title, length, yearOfRelease, ageLimit);
+    const length = document.getElementById('length').value;
+    const yearOfRelease = document.getElementById('yearOfRelease').value;
+    const ageLimit = document.querySelectorAll("input[name=ageLimit]:checked")[0].value;
 
-    //Vi laver nogle specifikationer til vores request
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json', // betyder == vi sender et json i string-format
-      },
-      body: body
-    };
+    console.log(ageLimit);
+    /*
+    if (document.getElementById('r1').checked) {
+      rate_value = document.getElementById('r1').value;
+    }
 
-    const url = 'http://localhost:8080/movies';
-
-    fetch(url, requestOptions)
-      .then(response => response.json())
-      .then(data =>  checkIfSuccess(data))
-      .catch(error => console.log("error: ", error));
-
-  });
-
-}
+     */
 
 
-/* metode fundet på:
-*
-* https://stackoverflow.com/questions/36280818/how-to-convert-file-to-base64-in-javascript
-* */
-function getBase64(file) {
-  return new Promise(function(resolve, reject) {
-    var reader = new FileReader();
-    reader.onload = function() { resolve(reader.result); };
-    reader.onerror = reject;
-    reader.readAsDataURL(file);
-  });
-}
+    const moviePromise = getBase64(moviePoster);
 
-// TODO rediger så den tager alle params
-function createJSONMovie(base64, title, length, yearOfRelease, ageLimit){
+    moviePromise.then(function (result) {
+      base64 = result;
 
-  const movie = {
-    'base64': base64,
-    'title': title,
-    'length': length,
-    'yearOfRelease': yearOfRelease,
-    'ageLimit': ageLimit,
-    'active': true
-  }
+      // TODO den skal tage alle movie-param
+      const body = createJSONMovie(base64, title, length, yearOfRelease, ageLimit);
 
-  return JSON.stringify(movie);
-}
+      //Vi laver nogle specifikationer til vores request
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json', // betyder == vi sender et json i string-format
+        },
+        body: body
+      };
 
-function checkIfSuccess(data){
+      const url = 'http://localhost:8080/movies';
 
-  if(data.id !== 0){
-    console.log("succes: " + data);
-    alert("Filmen " + data.title + " er nu oprettet succesfuldt");
-    console.log("agelimit " + data.ageLimit);
-    //window.location.replace('../general/add-shows-to-movie.html')
+      fetch(url, requestOptions)
+        .then(response => response.json())
+        .then(data => checkIfSuccess(data))
+        .catch(error => console.log("error: ", error));
+
+    });
 
   }
-  else{
-    alert("Der skete en fejl");
+
+
+  /* metode fundet på:
+  *
+  * https://stackoverflow.com/questions/36280818/how-to-convert-file-to-base64-in-javascript
+  * */
+  function getBase64(file) {
+    return new Promise(function (resolve, reject) {
+      var reader = new FileReader();
+      reader.onload = function () {
+        resolve(reader.result);
+      };
+      reader.onerror = reject;
+      reader.readAsDataURL(file);
+    });
+  }
+
+  // TODO rediger så den tager alle params
+  function createJSONMovie(base64, title, length, yearOfRelease, ageLimit) {
+
+    const movie = {
+      'base64': base64,
+      'title': title,
+      'length': length,
+      'yearOfRelease': yearOfRelease,
+      'ageLimit': ageLimit,
+      'active': true
+    }
+
+    return JSON.stringify(movie);
+  }
+
+  function checkIfSuccess(data) {
+
+    if (data.id !== 0) {
+      console.log("succes: " + data);
+      alert("Filmen " + data.title + " er nu oprettet succesfuldt");
+      console.log("agelimit " + data.ageLimit);
+      //window.location.replace('../general/add-shows-to-movie.html')
+
+    } else {
+      alert("Der skete en fejl");
+    }
   }
 }
-
 
 
 
